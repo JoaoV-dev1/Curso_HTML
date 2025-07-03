3b1ccef6d8932f6d8ce48ef31e81d2c3a257fbb6
joao victor de sousa <jvsp2006@gmail.com>
2025-07-03 18:48:50 -0300

    curso html

---
+++ outrooutro.js
@@ -0,0 +1,316 @@
+var treinoE = `<table class="abcde" id="peitoE">
+        <tr>
+            <th>Peito</th>
+        </tr>
+        <tr>
+            <td>
+                <ul>
+                    <li>Supino inclinado com halteres - 4x12</li>
+                    <li>Supino reto na maquina 3x10</li>
+                    <li>Crucifixo maquina 3x10</li>
+                    <li>Crossover na polia alta 3x10</li>
+                </ul>
+            </td>
+        </tr>
+    </table>
+    <table class="abcde" id="costas_e_abdomemE">
+        <tr>
+            <th>Costas</th>
+        </tr>
+        <tr>
+            <td>
+                <ul>
+                    <li>bi-set:<br>Puley pegada pronada 4x12(primeira serie aqucimento)<br>puley pegada supinada 3x8</li>
+                    <li>Remada baixa (no triangulo) 3x10</li>
+                    <li>Remada cavalinho (pegada aberta) 3x10</li>
+                    <li>Pulldown na polia 3x10</li>
+                    <li></li>
+                </ul>
+            </td>
+    </table>
+    <table class="abcde" id="pernasE">
+        <tr>
+            <th>Pernas</th>
+        </tr>
+        <tr>
+            <td>
+                <ul>
+                    <li>Agachamento 4x12</li>
+                    <li>Leg press 3x10</li>
+                    <li>Stiff 3x10</li>
+                    <li>Extensora 3x10</li>
+                    <li>Flexora 3x10</li>
+                </ul>
+            </td>
+        </tr>
+    </table>
+    <table class="abcde" id="triceps_e_ombroE">
+        <tr>
+            <th>Triceps e Ombro</th>
+        </tr>
+        <tr>
+            <td>
+                <ul>
+                    <li>Supino fechado 4x12</li>
+                    <li>Triceps testa 3x10</li>
+                    <li>Triceps pulley 3x10</li>
+                    <li>Desenvolvimento com halteres 3x10</li>
+                    <li>Elevaçao lateral com halteres 3x10</li>
+                </ul>
+            </td>
+        </tr>
+    </table>
+    <table class="abcde" id="biceps_e_antebraçoE">
+        <tr>
+            <th>Biceps e Antebraço</th>
+        </tr>
+        <tr>
+            <td>
+                <ul>
+                    <li>Rosca direta 4x12</li>
+                    <li>Rosca alternada 3x10</li>
+                    <li>Rosca concentrada 3x10</li>
+                    <li>Rosca inversa 3x10</li>
+                    <li>Encolhimento de ombros com halteres 3x10</li>
+                </ul>
+            </td>
+        </tr>
+
+    </table>`;
+
+    var treinoPPL = `
+        <table  class="PPL" id="pull">
+        <tr>
+            <th>pull</th>
+        </tr>
+            <td>
+                <ul>
+                    <li>pulley pronada e supinada(bi-set) 3x10</li>
+                    <li>Remada baixa no triangulo 3x10</li>
+                    <li>Pulldown 3x10</li>
+                    <li>Rosca scott 3x10</li>
+                    <li>Rosca alternada com halter 3x10</li>
+                    <li>(tri-set)<br>abdominal supra(30-50 reps), prancha(30-120seg) <br> e abdominal infra(10-20 reps)</li>
+                </ul>
+            </td>
+        </table>
+        <table  class="PPL" id="legs">
+        <tr>
+            <th>legs</th>
+        </tr>
+            <td>
+                <ul>
+                    <li>Agachamento 3x15</li>
+                    <li>Leg press 3x10</li>
+                    <li>Stiff 3x10</li>
+                    <li>Extensora 3x20</li>
+                    <li>Flexora 3x10</li>
+                    <li>Panturrilha burrinho 3x10</li>
+                </ul>
+            </td>
+        </table>
+        <table class="PPL" id="push">
+        <tr>
+            <th>push</th>
+        </tr>
+            <td>
+                <ul>
+                    <li>Supino reto 3x10</li>
+                    <li>Supino inclinado 3x10</li>
+                    <li>cruxifixo 3x10</li>
+                    <li>Triceps francês 3x10</li>
+                    <li>Triceps pulley 3x10</li>
+                    <li>Desenvolvimento na maquina 3x10</li>
+                    <li>Elevação frontal 3x10</li>
+                </ul>
+    </table>
+    `
+    var treinoD = `
+            <table class="abcd" id="costasD">
+            <tr>
+                <th>costas,biceps e abdomem</th>
+            </tr>      
+                <td>
+                    <ul>
+                        <li>pulley pronada e supinada(bi-set) 3x10</li>
+                        <li>Remada baixa no triangulo 3x10</li>
+                        <li>Pulldown 3x10</li>
+                        <li>Rosca scott 3x10</li>
+                        <li>Rosca alternada com halter 3x10</li>
+                        <li>Tri-set:<br>abdominal supra(30-50 reps)<br> prancha(30-120seg)<br>abdominal infra(10-20 reps) (tri-set)</li>
+                    </ul>
+                </td>
+            </table>
+            <table class="abcd" id="triceps_e_ombrosD">
+            <tr>
+                <th>ombros e triceps</th>
+            </tr>
+                <td>
+                    <ul>
+                        <li>Desenvolvimento com halteres 3x10</li>
+                        <li>Elevação lateral 3x10</li>
+                        <li>Elevação frontal 3x10</li>
+                        <li>Tríceps frances 3x10</li>
+                        <li>Tríceps pulley 3x10</li>
+                        <li>Tríceps testa 3x10</li>
+                    </ul>
+                </td>
+            </table>
+            <table class="abcd" id="pernasD">
+            <tr>
+                <th>pernas</th>
+            </tr>
+                <td>
+                    <ul>
+                        <li>Agachamento 3x15</li>
+                        <li>Leg press 3x10</li>
+                        <li>Stiff 3x10</li>
+                        <li>Extensora 3x20</li>
+                        <li>Flexora 3x10</li>
+                        <li>Panturrilha burrinho 3x10</li>
+                    </ul>
+                </td>
+            </table>
+            <table class="abcd" id="peitoD">
+            <tr>
+                <th>peito</th>
+            </tr>
+                <td>
+                    <ul>
+                        <li>Supino inclinado com halteres 3x10</li>
+                        <li>Supino reto 3x10</li>
+                        <li>Cruxifixo 3x10</li>
+                        <li>Cruxifixo adaptado para parte superior 3x10</li>
+                        <li>Paralela 3x10</li>
+                    </ul>
+                </td>
+            </table>
+        `;    
+
+var treinoUL = `
+<table class="UL" id="upper">
+    <tr>
+        <th>upper</th>
+    </tr>
+        <td>
+            <ul>
+                <li>Supino inclinado com halteres 3x10</li>
+                <li>supino reto na maquina 3x10</li>
+                <li>pulley pegada pronada 3x10</li>
+                <li>remada baixa no triangulo 3x10</li>
+                <li>pulldown 3x10</li>
+                <li>triceps frances 3x10</li>
+                <li>biceps scott 3x10</li>
+                <li>desenvolvimento 3x10</li>
+                <li>elevação frontal 3x10</li>
+            </ul>
+        </td>
+</table>
+<table class="UL" id="lower">
+    <tr>
+        <th>lower</th>
+    </tr>
+        <td>
+            <ul>
+                <li>agachamento 3x10</li>
+                <li>leg press 3x10</li>
+                <li>stiff 3x10</li>
+                <li>panturrilha burrinho 3x10</li>
+                <li>panturrilha em pé 3x10</li>
+                <li>flexora 3x10</li>
+                <li>extensora 3x10</li>
+                <li>abdominal supra 3x10</li>
+                <li>prancha 3x quanto tempo voce conseguir</li>
+            </ul>
+        </td>
+</table>
+`;
+
+
+var genero = "";
+
+function funcao() {
+    const iniciar = document.getElementById("iniciar");
+    iniciar.innerHTML = "";
+    iniciar.style.display = "none";
+    const trembolona = document.getElementById("trembolona");
+    trembolona.innerHTML = `
+    <p>Escolha seu gênero:</p>
+    <button onclick="guardarGenero('masc')">masculino</button>
+    <button onclick="guardarGenero('fem')">feminino</button>`;
+}
+
+function treino(t) {
+    console.log("Treino selecionado: " + t);
+    const trembolona = document.getElementById("trembolona");
+    trembolona.innerHTML = "";
+    trembolona.style.display = "none";
+
+    switch (t) {
+        case "ABCD":
+            trembolona.innerHTML = `
+            <p>Treino ABCD:</p>
+            <div class="div_treino">
+            ${treinoD}
+            </div>
+            <p>Esse treino funciona da seguinte forma:<br>
+            - 2 dias de treino, 1 de descanso.</p>
+            `
+            
+            break;
+        case "ABCDE":
+            trembolona.innerHTML = `
+            <p>Treino ABCDE:</p>
+            <div class="div_treino">
+            ${treinoE}
+            </div>
+            <p>Esse treino funciona da seguinte forma:<br>
+            - 5 dias de treino, 2 de descanso.</p>
+            `
+            
+            break;
+        case "PPL":
+            trembolona.innerHTML = `
+            <p>Treino PPL:</p>
+            <div class="div_treino">
+            ${treinoPPL} 
+            </div>
+            <p>Esse treino funciona da seguinte forma:<br>
+            - 3 dias de treino, 1 de descanso.<br>
+           `
+            
+            break;
+        case "UL":
+            trembolona.innerHTML = `
+            <p>Treino UL:</p>
+            <div class="div_treino">
+            ${treinoUL}
+            </div>
+            <p>Esse treino funciona da seguinte forma:<br>
+            - 2 dias de treino, 1 de descanso.<br>
+            `
+            
+            break;
+        default:
+            break;
+    }
+    trembolona.style.display = "block";
+}
+
+function guardarGenero(g) {
+    genero = g;
+    console.log("Genero guardado: " + genero);
+
+    const trembolona = document.getElementById("trembolona");
+    trembolona.innerHTML = "";
+    trembolona.style.display = "none";
+
+    trembolona.innerHTML = `
+    <p>escolha seu treino:</p>
+    <button onclick="treino('ABCD')">ABCD</button>
+    <button onclick="treino('ABCDE')">ABCDE</button>
+    <button onclick="treino('PPL')">PPL</button>
+    <button onclick="treino('UL')">UL</button>
+    `;
+    trembolona.style.display = "block";
+}
